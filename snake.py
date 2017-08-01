5347b07a7eb70cb37902f2d85d5914683f26c82e
noamm19-meet <noamm19@meet.mit.edu>
2017-08-01 14:26:09 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -32,36 +32,103 @@ for num in range(0,START_LENGHT):
     pos_list.append(my_pos)
     new_stamp= snake.stamp()
     stamp_list.append(new_stamp)
+    
 UP_ARROW="Up"
 LEFT_ARROW="Left"
 DOWN_ARROW="Down"
 RIGHT_ARROW="Right"
+TIME_STEP= 100
 SPACEBAR="space"
-UP=0
+
 
 UP=0
 LEFT=1
 DOWN=2
 RIGHT=3
+direction = UP
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
 
 def up():
     global direction
-    direction=UP
-    move_snake()
+    direction=UP 
     print('you pressed up')
 
 def left():
     global direction
     direction=LEFT
-    move_snake()
     print('you pressed left')
 
 def down():
     global direction
     direction=DOWN
-    move_snake()
     print('you pressed down')
 
+    
+def right():
+    global direction
+    direction=RIGHT
+    print('you pressed right')
+
+turtle.onkeypress(up , UP_ARROW)
+turtle.onkeypress(left , LEFT_ARROW)
+turtle.onkeypress(down , DOWN_ARROW)   
+turtle.onkeypress(right , RIGHT_ARROW)
+turtle.listen()
+
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+
+    if direction==RIGHT:
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print('you moved right')
+    elif direction==LEFT:
+        snake.goto(x_pos - SQUARE_SIZE, y_pos) 
+        print('you moved left')
+    elif direction==UP:
+        snake.goto(x_pos ,SQUARE_SIZE + y_pos)
+        print('you moved up')
+    else:
+         snake.goto(x_pos , y_pos - SQUARE_SIZE)
+         print('you moved down')
+
+         
+    my_pos=snake.pos()
+    pos_list.append(my_pos)
+    new_stamp=snake.stamp()
+    stamp_list.append(new_stamp)
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+
+    if new_x_pos >= RIGHT_EDGE:
+        print('GAME OVER!')
+        quit()
+    elif new_x_pos <= LEFT_EDGE:
+        print('GAME OVER')
+        quit()
+    elif new_y_pos >= UP_EDGE:
+        print('GAME OVER')
+        quit()
+    elif new_y_pos <= DOWN_EDGE:
+        print('GAME OVER')
+        quit()
+
+    turtle.ontimer(move_snake, TIME_STEP)
+
+move_snake()
+
+
+         
+    
     
     
 
